495e375a64a4d43faa7a5c0a8168ee3b427e0039
Juwon Hwang <juwon0018@postech.ac.kr>
2022-05-03 14:45:13 +0900

    arrange code

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from psychopy import visual, event, data, core, sound
 from random import *
 
 sample_sound = sound.Sound('I.wav')
-
 ws = [1536, 864]
 black = [-1,-1,-1]
+wait_time = 0.5
+win = visual.Window(ws,[30,30],color=black, fullscr=True)
+
 
 def wp(position):
     return [position[0]/ws[0],position[1]/ws[1]]
@@ -14,33 +16,41 @@ def drawlines(_lines):
     for line in _lines:
         line.draw()
 
-win = visual.Window(ws,[30,30],color=black, fullscr=True)
-
-stim_text = 'Loud / Small'
-message = visual.TextStim(win, font='Malgun Gothic', color = 'white', text = stim_text) 
-message.draw()
-win.flip()
-
-event.waitKeys()
+def imagine(_win,_lines):
+    drawlines(_lines)
+    _win.flip()
+    core.wait(wait_time)
+    _win.flip()
+    core.wait(wait_time)
+    
 
 linelength = 30
 lines = [visual.Line(win, start=wp([0,linelength]), end=wp([0,-linelength])),
         visual.Line(win, start=wp([linelength,0]), end=wp([-linelength,0]))]
-        
-drawlines(lines)
-        
-win.flip()
-nextFlip = win.getFutureFlipTime(clock='ptb')
-sample_sound.play(when=nextFlip)
-
-core.wait(2)
-
+stim_text = 'Loud / Small'
+message = visual.TextStim(win, font='Malgun Gothic', color = 'white', text = stim_text) 
 select_text = '1 / 2'
 select_message = visual.TextStim(win, font='Malgun Gothic', color = 'white', text = select_text) 
-select_message.draw()
 
+# select loud or small
+message.draw()
 win.flip()
-
 event.waitKeys()
 
+# random flash
+n = randint(1,6)
+for i in range(n):
+    imagine(win,lines)
+    
+# play sound 
+nextFlip = win.getFutureFlipTime(clock='ptb')
+sample_sound.play(when=nextFlip)
+core.wait(2)
+
+# select level 
+select_message.draw()
+win.flip()
+event.waitKeys()
+
+# close
 win.close()
