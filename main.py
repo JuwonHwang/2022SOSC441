0ae22b2a4237545dfc363d8c4735955bb8ae4171
Juwon Hwang <juwon0018@postech.ac.kr>
2022-05-09 19:25:33 +0900

    replace sound & tell sample in the first time

--- main.py
+++ main.py
@@ -4,9 +4,18 @@ from random import *
 import csv
 from datetime import datetime
 
-sample_sound = [sound.Sound('sounds/s'+str(i+1)+'.wav') for i in range(5)]
+class Condition:
+    def __init__(self):
+        self.loudness = [0,1] # 0 : loud , 1 : soft
+        self.intensity = [1,2,3,4,5]
+        self.repetition = [1,2,3,4,5,6]
+        self.cases = [tuple([i,j,k]) for i in self.loudness for j in self.intensity for k in self.repetition]
+        shuffle(self.cases)
+
+sample_sound = [sound.Sound('sounds/s'+str(i)+'.wav') for i in range(5)]
 ws = [1536, 864]
 win = visual.Window(ws,[30,30],color='black', fullscr=True)
+now = datetime.now().strftime('%m_%d %H_%M')
 
 def wp(position):
     return [position[0]/ws[0],position[1]/ws[1]]
@@ -15,11 +24,17 @@ def drawlines(_lines):
     for line in _lines:
         line.draw()
 
-def imagine(_win,_lines, wait_time):
+def imagine(_lines, wait_time):
     drawlines(_lines)
-    _win.flip()
+    win.flip()
     core.wait(wait_time)
-    
+   
+def waitEsc():
+    keys = event.waitKeys() # until button press
+    if 'escape' in keys:
+        win.close()
+        exit()
+ 
 
 linelength = 60
 lines = [visual.Line(win, start=wp([0,linelength]), end=wp([0,-linelength]), lineColor='white'),
@@ -30,34 +45,25 @@ redlines = [visual.Line(win, start=wp([0,linelength]), end=wp([0,-linelength]), 
 
 select_message = visual.TextStim(win, font='Malgun Gothic', color = 'white', text = '1  2  3  4  5', height=0.2)
 
-class Condition:
-    def __init__(self):
-        self.loudness = [0,1] # 0 : loud , 1 : soft
-        self.intensity = [1,2,3,4,5]
-        self.repetition = [1,2,3,4,5,6]
-        self.cases = [tuple([i,j,k]) for i in self.loudness for j in self.intensity for k in self.repetition]
-        shuffle(self.cases)
+cnd = Condition()
 
 def isloud(a):
     return 'loud' if a == 0 else 'soft'
 
 def block(writer):
-    cnd = Condition()
     for loudness, intensity, repetition in cnd.cases:
         # loudness imagery cue determination 
         message = visual.TextStim(win, font='Malgun Gothic', color = 'white', text = isloud(loudness), height=0.3)
         message.draw()
         win.flip()
-        keys = event.waitKeys() # until button press
-        if 'escape' in keys:
-            return 1
+        waitEsc()
         
         for i in range(repetition): # repetetion
-            imagine(win, lines, 0.5) # cross line
+            imagine(lines, 0.5) # cross line
             win.flip()
             core.wait(0.5)
     
-        imagine(win, redlines, 1) # cross red line
+        imagine(redlines, 1) # cross red line
         win.flip()
         
         # play sound 
@@ -71,7 +77,7 @@ def block(writer):
         win.flip()
         judgement = event.waitKeys(keyList=['1','2','3','4','5'], modifiers=False)[0]
         # csv write
-        writer.writerow({'loudness': loudness,
+        writer.writerow({'loudness': isloud(loudness),
                          'intensity': intensity,
                          'repetition': repetition,
                          'judgement': int(judgement)})
@@ -89,15 +95,32 @@ while 'return' not in keys:
     textBox.draw()
     win.flip()
 
-name = textBox.text[:-1]
+sample_text = 'We will present you with 5 levels of sound of increasing magnitude.'
+sample_massage = visual.TextStim(win, font='Malgun Gothic', color = 'white', text = sample_text, height=0.15)
+sample_massage.draw()
+win.flip()
 
-now = datetime.now().strftime('%m_%d %H_%M')
+waitEsc()
+
+for i in cnd.intensity:
+    intensity_massage = visual.TextStim(win, font='Malgun Gothic', color = 'white', text = str(i), height=0.5)
+    intensity_massage.draw()
+    win.flip()
+    nextFlip = win.getFutureFlipTime(clock='ptb')
+    sample_sound[i-1].play(when=nextFlip)
+    core.wait(0.5)
+    win.flip()
+    core.wait(0.5)
+
+waitEsc()
+
+name = textBox.text.replace('\n','')
 with open('result/' + name + '_' + now + '.csv', 'w', newline='') as csvfile:
     fieldnames = ['loudness', 'intensity', 'repetition', 'judgement']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
-    
     block(writer)
 
 # close
 win.close()
+exit()
